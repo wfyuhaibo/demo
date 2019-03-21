3a46c4f993cbf90fa1916a9e0288cb3d862f38c5
yuhaibo <1724852741@qq.com>
2019-03-22 01:34:18 +0800

    first commit

---
+++ src/main/java/com/yu/demo/Hello.java
@@ -0,0 +1,14 @@
+package com.yu.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class Hello {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello for mac";
+    }
+}
